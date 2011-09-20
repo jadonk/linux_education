ae048f3482170f4ee1d73e802ebf67ad647c7e8a
Jason Kridner <jdk@ti.com>
2011-09-20 19:36:21 -0400

    Made Cloud9 IDE easier to install and run

--- cloud9.sh
+++ cloud9.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 cd $(dirname $0)
 DIR=`pwd`
+if [ ! -e cloud9/bin/cloud9.js ]; then 
+ echo Fetching Cloud9 IDE...
+ git submodule update --init --recursive cloud9/
+fi
 echo Starting Cloud9 IDE at $DIR
-$DIR/cloud9/bin/cloud9.sh -w $DIR
+node cloud9/bin/cloud9.js -w . -l 0.0.0.0 -p 3000
